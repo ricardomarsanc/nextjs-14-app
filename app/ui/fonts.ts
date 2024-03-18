b25b26e05ffc470983233710d28c09c855f3c08c
Ric <ricardomarsanc@gmail.com>
2024-03-18 20:04:04 +0100

    updated fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
-import { Inter, Montserrat } from 'next/font/google'
+import { Inter, Lusitana } from 'next/font/google'
 
 export const inter = Inter({ subsets: ['latin'], display: 'swap' })
-export const montserrat = Montserrat({ subsets: ['latin'] })
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] })
