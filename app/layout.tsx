0f6f0705eb5c864b646d7c3f31a7f3509bd79d66
Ric <ricardomarsanc@gmail.com>
2024-03-29 19:09:32 +0100

    Añadir metadatos y optimizar imágenes en el diseño de la página

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Invoiceitor 3000',
+    default: 'Invoiceitor 3000',
+  },
+  description: 'The best Invoice management tool of the year.',
+  metadataBase: new URL('https://nextjs-14-app-seven.vercel.app/'),
+}
 
 export default function RootLayout({
   children,
@@ -9,8 +19,17 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.className} antialiased`}>
       <head>
-        <title>My React Page</title>
-        <link rel="icon" type="image/x-icon" href="../public/favicon.ico" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <meta property="og:title" content="Invoiceitor 3000" />
+        <meta
+          property="og:description"
+          content="The best platform for invoices"
+        />
+        <meta property="og:image" content="../public/opengraph-image.png" />
+
+        <meta name="keywords" content="invoices, invoiceitor, papaya" />
       </head>
       <body>{children}</body>
     </html>
